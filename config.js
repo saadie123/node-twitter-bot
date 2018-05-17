f733f94a38d67e8af4254844c7ff376ba59f6edf
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-05-18 03:06:06 +0500

    installed twit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'FofOLn05Uec4ixhKKogMO57iC',
+    consumer_secret: 'yT1tVDc2p9GxOVilC1yFtZkpBWzr3asCBRj4scglAvZoPnn53M',
+    access_token: '2309680898-lUshXQrsdHIVRrrjBDChn1DK0xedNRn6v4oHzqC',
+    access_token_secret: 'DQxrtiT07GKis4EkBrdXBx3yIpuNCgTmJnxyMY59ZWNaQ'
+}
